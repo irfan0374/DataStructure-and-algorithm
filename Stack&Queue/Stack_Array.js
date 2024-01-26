b8f6565356446a6e688d99c612c246f988667a09
Irfan <irfan188iqbal@gmail.com>
2024-01-27 01:32:23 +0530

    hashTable

--- Stack&Queue/Stack_Array.js
+++ Stack&Queue/Stack_Array.js
@@ -1,42 +1,99 @@
-class Stack{
-    constructor(){
-        this.top=-1;
-        this.stack=[]
-        this.b=[]
+// // stack in array
+
+// class Stack {
+//     constructor() {
+//         this.top = -1;
+//         this.stack = []
+//         this.b = []
+//     }
+//     isEmpty() {
+//         return this.top == -1;
+//     }
+//     peek() {
+//         return this.stack[this.top]
+//     }
+//     redo() {
+//         this.b.push(this.stack.pop())
+//         this.top--;
+//     }
+//     undo() {
+//         this.stack.push(this.b.pop())
+//         this.top++;
+//     }
+//     push(value) {
+//         this.top++
+//         this.stack[this.top] = value;
+//     }
+//     pop() {
+//         if (this.isEmpty()) {
+//             return console.log("list is empty")
+//         }
+//         const pop = this.stack[this.top];
+//         this.top--;
+//         return pop
+//     }
+
+//     print() {
+//         if (this.isEmpty()) {
+//             return console.log("list is empty")
+//         }
+//         for (let i = this.top; i >= 0; i--) {
+//             console.log(this.stack[i])
+//         }
+//     }
+// }
+
+// queue in array
+
+class queue {
+    constructor() {
+        this.queue = [];
+        this.front = -1;
+        this.rear = -1
+
     }
     isEmpty(){
-        return this.top==-1;
+        return this.front==-1
     }
+    
     peek(){
-        return this.stack[this.top]
+        return this.queue[this.front]
     }
-    redo(){
-        this.b.push(this.stack.pop())
-        this.top--;
-    }
-    undo(){
-        this.stack.push(this.b.pop())
-        this.top++;
-    }
-    push(value){
-        this.top++
-        this.stack[this.top]=value;
-    }
-    pop(){
+    enqueue(value){
         if(this.isEmpty()){
-            return console.log("list is empty")
+            this.front=0;
+            this.rear=0;
+        }else{
+            this.rear++;
         }
-        const pop=this.stack[this.top];
-        this.top--;
-        return pop
+        this.queue[this.rear]=value
+    }
+    dequeue(){
+        if(this.isEmpty()){
+            return console.log("queue is empty")
+        }else{
+            const dequeue=this.front;
+            this.front++;
+            return dequeue
+        }
     }
 
     print(){
         if(this.isEmpty()){
-            return console.log("list is empty")
-        }
-        for(let i=this.top;i>=0;i--){
-            console.log(this.stack[i])
+            return console.log("queue is empty");
+        }else{
+            for(let i=this.front;i<=this.rear;i++){
+                console.log(this.queue[i])
+            }
         }
     }
 }
+
+const list=new queue()
+list.enqueue(1)
+list.enqueue(2)
+list.enqueue(3)
+list.enqueue(4)
+list.enqueue(5)
+list.print()
+console.log(list.peek())
---
+++ hashTable.js
@@ -0,0 +1,54 @@
+class hashTable{
+    constructor(size){
+        this.table=new Array(size)
+        this.size=size
+    }
+    hash(key){
+        let total=0;
+        for(let i=0;i<key.length;i++){
+            total+=key.charCodeAt(i)
+        }
+        return total%this.size
+    }
+    set(key,value){
+        let index=this.hash(key)
+        let bucket=this.table[index]
+        if(!bucket){
+            this.table[index]=[[key,value]];
+            
+        }else{
+            const sameIndex=bucket.find((item)=>item[0]==key)
+            if(sameIndex){
+                sameIndex[1]=value
+
+            }else{
+                bucket.push([key,value])
+            }
+        }
+    }
+
+    get(key){
+        let index=this.hash(key)
+        let bucket=this.table[index];
+        if(bucket){
+            let sameIndex=bucket.find((item)=>item[0]==key)
+            if(sameIndex){
+
+                return sameIndex[1]
+            }
+        }
+        return undefined
+    }
+    print(){
+        for(let i=0;i<this.table.length;i++){
+            console.log(i,this.table[i])
+        }
+    }
+}
+
+const hash=new hashTable(10)
+hash.set("name","irfan")
+hash.set("age",23)
+hash.set("emna","amaan")
+hash.print()
+console.log(hash.get("name"))
